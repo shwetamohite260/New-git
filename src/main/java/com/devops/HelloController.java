ddb5642a69616e064cb0ea04fde7a37bde83da55
Abhijeet-Mohite <abhijitmohite28@gmail.com>
2026-03-29 08:25:52 +0530

    Initial commit - DevOps project

---
+++ src/main/java/com/devops/HelloController.java
@@ -0,0 +1,13 @@
+package com.devops;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello DevOps Project Running!";
+    }
+}
